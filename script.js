4d8d2368c3734415c266d6c233a91bfd9f5e0b06
Daniil Panov <from4life@pm.me>
2023-06-11 15:34:58 +0700

    Create a recursive function

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 console.log('recursion rocks!');
 
-const fib = (length) => {
+const fib = (num) => {
     let numArr = [];
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < num; i++) {
         if (numArr.length === 0 || numArr.length === 1) {
             numArr.push(numArr.length);
         } else {
-            numArr.push((numArr[numArr.length - 1]) + (numArr[numArr.length - 2]))
+            numArr.push(numArr[numArr.length - 1] + numArr[numArr.length - 2]);
         }
     }
     console.log(numArr);
 }
 
-// fib(8);
+const fibsRec = (num) => {
+    let numArr = [];
+    if (num === 1) {
+        numArr.push(0);
+        return numArr;
+    } 
+    if (num === 2) {
+        numArr.push(0,1);
+        return numArr;
+    } else {
+        numArr = fibsRec(num-1);
+        return numArr.concat(numArr[numArr.length - 1] + numArr[numArr.length - 2]);
+    }
+}
 
-// const fibsRec = (length) => {
-//     let numArr = [];
-//     if (length === 1) {
-//         return numArr.push(0);
-//     } else if (length === 2 && numArr[0] === 0) {
-//         return numArr.push(1);
-//     } else {
-//         numArr.push(numArr[numArr.length - 1] + numArr[numArr.length - 2]);
-//     }
-//     console.log(numArr)
-// }
-
-// // fibsRec(2)
-
-// let arr = [];
-// console.log(arr.length + arr.length - 1)
+// fibsRec(8);
+console.log(fibsRec(8));
