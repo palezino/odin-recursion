cdda86e90b2588cd1b4769d90f87bbeb94ad3c63
Daniil Panov <from4life@pm.me>
2023-06-11 14:11:27 +0700

    Change the function with 'for' loop

--- script.js
+++ script.js
@@ -2,16 +2,31 @@ console.log('recursion rocks!');
 
 const fib = (length) => {
     let numArr = [];
-    let num = 0;
     for (let i = 0; i < length; i++) {
-        if( numArr.length < 2) {
-            numArr.push(i)
+        if (numArr.length === 0 || numArr.length === 1) {
+            numArr.push(numArr.length);
         } else {
-            num = numArr[i - 1] + numArr[i - 2];
-            numArr.push(num);
+            numArr.push((numArr[numArr.length - 1]) + (numArr[numArr.length - 2]))
         }
     }
     console.log(numArr);
 }
 
-fib(8);
+// fib(8);
+
+// const fibsRec = (length) => {
+//     let numArr = [];
+//     if (length === 1) {
+//         return numArr.push(0);
+//     } else if (length === 2 && numArr[0] === 0) {
+//         return numArr.push(1);
+//     } else {
+//         numArr.push(numArr[numArr.length - 1] + numArr[numArr.length - 2]);
+//     }
+//     console.log(numArr)
+// }
+
+// // fibsRec(2)
+
+// let arr = [];
+// console.log(arr.length + arr.length - 1)
